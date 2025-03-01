f0139566a4f5e76b652d2b759f3d6a04fcd0d169
Elfred Bayona <elfredmbayona@gmail.com>
2025-03-01 23:06:59 +0800

    media 468

--- FBC_WEB/js/menuToggle.js
+++ FBC_WEB/js/menuToggle.js
@@ -13,3 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+function toggleProcedure(button) {
+    let flowchart = button.nextElementSibling;
+    if (flowchart.classList.contains("show")) {
+        flowchart.classList.remove("show");
+        flowchart.classList.add("hide");
+        
+        setTimeout(() => {
+            flowchart.style.display = "none";
+            flowchart.classList.remove("hide");
+        }, 500); // Wait for animation to complete
+    } else {
+        flowchart.style.display = "flex";
+        flowchart.classList.remove("hide");
+        flowchart.classList.add("show");
+    }
+}
